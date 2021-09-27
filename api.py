df34c260a2579912bb0ad743bf3cef39f3296c3b
dydtmd6801 <dydtmd6801@naver.com>
2021-09-27 04:20:13 -0700

    yes

---
+++ api.py
@@ -0,0 +1,10 @@
+from logging import debug
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/test', methods=['post'])
+def test():
+    return {"reuslt" : "test_success"}
+
+app.run(host='0.0.0.0', port=8080, debug=True)
