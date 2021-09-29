2e6ecbe20236d52ecdaf036d41cca6bcec3693be
dydtmd6801 <dydtmd6801@naver.com>
2021-09-29 02:48:06 -0700

    checkdigit_api

--- api.py
+++ api.py
@@ -1,10 +1,51 @@
-from logging import debug
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/test', methods=['post'])
+@app.route('/test', methods=['POST'])
 def test():
-    return {"reuslt" : "test_success"}
+    test = request.get_json()
+    print(test)
+    if not "key" in test:
+        return {"reuslt" : "test_success"}
+    else :
+        return {"reuslt" : "test_fail"}
+
+
+@app.route('/cdg', methods=['POST'])
+def cdg():
+    result = 0
+    cnt = 0
+    user_data = request.get_json()
+    print(user_data)
+    if user_data:
+        if "number" in user_data:
+            if user_data["number"]:
+                user_data_result = user_data["number"].replace("-","")
+                print(user_data_result)
+                for i in range(2,10):
+                    result += int(user_data_result[cnt]) * i
+                    print(user_data_result[cnt], "X", i, "=", result)
+                    cnt += 1
+                for i in range(2,6):
+                    result += int(user_data_result[cnt]) * i
+                    print(user_data_result[cnt], "X", i, "=", result)
+                    cnt += 1
+                print("result : ", result)
+                result = result % 11
+                result = result - 11
+                print("result", abs(result))
+                print("user_data[12]", user_data_result[12])
+                if(int(abs(result)) == int(user_data_result[12])):
+                    return {"kimsu_is_good_teacher":"so delicious"}
+                else:
+                    return {"cdg":"no_cdg"}
+            else:
+                return {"value_error":"value_is_empty"}
+        else:
+            return {"key_error":"key = number"}
+    else:
+        return {"status":"fail"}
+        
 
 app.run(host='0.0.0.0', port=8080, debug=True)
